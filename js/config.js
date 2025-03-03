34a14b6a668e030af6e15ba1b9eb22b755ec1019
prganalyst <learneralwz@gmail.com>
2025-03-03 11:41:43 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-2_CRjFZNInx', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5ib5nsn2b2l67ssa68rqm466k5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-west-2_CRjFZNInx', // e.g. us-east-2_ziMiOmt0a
+        userPoolClientId: '3b98vkpupcffj8ibp6rbvbq7ho', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-west-2' // e.g. us-east-2
     },
     api: {
